a2f170910ca66b31169fa423736f0a0ca7f8f179
Abdulazeez <41235090+AbdulazeezAde@users.noreply.github.com>
2024-04-19 18:39:09 +1000

    Update utils.py

--- utils.py
+++ utils.py
@@ -47,7 +47,7 @@ def get_answer(messages):
     memory = conversation_chain.memory
     if isinstance(memory, ConversationBufferMemory):
         memory.clear()
-    return response.chat_history[1]
+    return response["answer"]
 
 
 def speech_to_text(audio_data):
