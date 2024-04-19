9eeb7e28c99db07021b5966b57b2fe62a3747f26
Abdulazeez <41235090+AbdulazeezAde@users.noreply.github.com>
2024-04-19 15:53:30 +1000

    Update utils.py

--- utils.py
+++ utils.py
@@ -40,9 +40,6 @@ def get_conversation_chain(vector_store):
 
 
 def get_answer(messages):
-    #llm = ChatOpenAI(model_name="gpt-3.5-turbo-1106", temperature=0, openai_api_key=api_key)
-    #memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
-    #conversation_chain = ConversationalRetrievalChain.from_llm(llm=llm, retriever=vector_store.as_retriever(), memory=memory)
     system_message = {"role": "system", "content": "You are a helpful AI chatbot that answers questions asked by users about Swim Schools."}
     messages.append(system_message)
     user_question = messages[-2]["content"]
